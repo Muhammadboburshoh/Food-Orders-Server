d489ba0da3900d4ded5135da8c46315355396c7a
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-06-09 13:48:50 +0500

    Add try catch

--- src/modules/orders/controllers.js
+++ src/modules/orders/controllers.js
@@ -7,7 +7,14 @@ const order = require("./model")
 */
 router.post("/", async (req, res) => {
 
-  res.send(await order.order(req.body))
+  try {
+
+    res.send(await order.order(req.body))
+  } catch(e) {
+    console.log(e)
+    res.statusMessage = e.message
+    res.status(401).end()
+  }
 })
 
 module.exports = router
