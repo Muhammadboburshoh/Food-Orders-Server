091f06ab91d68d9c5181762b355bb1c8e838fbad
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-06-09 11:05:24 +0500

    create all products page

---
+++ setup/mock.sql
@@ -0,0 +1,25 @@
+insert into categories (category_name) values
+('Uyg''ur'),
+('Japan'),
+('Milliy'),
+('Turk'),
+('Ichimlik')
+;
+
+insert into products (
+  product_name,
+  product_price,
+  product_image,
+  category_id
+) values
+('Osh', '25000', 'osh.png', 3),
+('Uyg''urcha qovurma', '30000', 'u_qovurma.png', 1),
+('Manti', '5000', 'manti.png', 3),
+('Ko''za sho''rva', '25000', 'k_shorva.png', 3),
+('Sushe', '56000', 'pushe.png', 2),
+('Pepsi', '11000', 'pepsi.png', 5),
+('Fanta', '11000', 'fanta.png', 5),
+('Aliviya', '18000', 'aliviya.png', 3)
+;
+
+insert into tables(table_number) values (1), (2), (3), (4), (5);
--- src/modules/products/model.js
+++ src/modules/products/model.js
@@ -1,11 +1,31 @@
 const { rows, row } = require("../../../util/database")
 
-const GET_ALL = `
-  select * from products;
+const productsSQL = `
+  select
+    p.product_id,
+    p.product_name,
+    p.product_price,
+    p.product_image,
+    p.product_description,
+    p.product_available,
+    c.category_name
+  from
+    products as p
+  join
+    categories as c on c.category_id = p.category_id and c.category_id = $1
 `
 
-const getAll = () => {
-  return row(GET_ALL)
+const tablesSQL = `
+  select * from tables
+`
+
+const getAll = (catigoryId) => {
+  return rows(productsSQL, catigoryId)
+}
+
+const tables = () => {
+  return rows(tablesSQL)
 }
 
 module.exports.getAll = getAll
+module.exports.tables = tables
