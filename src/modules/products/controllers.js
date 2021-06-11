4b05c7b37531b44694a333aa377df078938b9aea
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-06-11 22:47:27 +0500

    Edit all products api

--- src/modules/products/controllers.js
+++ src/modules/products/controllers.js
@@ -9,29 +9,22 @@ const products = require("./model")
 
 
 /* GET all products. */
-router.get('/', async function(req, res, next) {
+router.get('/:id', async function(req, res, next) {
 
-  const tables = await products.tables()
-
+  /* const tables = await products.tables()
   const uygurProducts = await products.getAll(1)
-
   const japanProducts = await products.getAll(2)
-
   const milliyProducts = await products.getAll(3)
-
   const turkProducts = await products.getAll(4)
+  const ichimlikProducts = await products.getAll(5) */
+  const catigoryId = req.params.id
+  const catigoryProducts = await products.getAll(catigoryId - 0)
 
-  const ichimlikProducts = await products.getAll(5)
-
-  res.send({
-      tables,
-      uygurProducts,
-      japanProducts,
-      milliyProducts,
-      turkProducts,
-      ichimlikProducts
-  })
-  res.status(401).end()
+  if(catigoryProducts) {
+    res.status(201).send(catigoryProducts)
+  } else {
+    res.status(401).end()
+  }
 })
 
 
