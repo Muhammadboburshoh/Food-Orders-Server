a160de029284b7de41edcdedb3aa64bf725865db
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-06-13 18:42:24 +0500

    Edit products page

--- src/modules/products/controllers.js
+++ src/modules/products/controllers.js
@@ -11,12 +11,6 @@ const products = require("./model")
 /* GET all products. */
 router.get('/:id', async function(req, res, next) {
 
-  /* const tables = await products.tables()
-  const uygurProducts = await products.getAll(1)
-  const japanProducts = await products.getAll(2)
-  const milliyProducts = await products.getAll(3)
-  const turkProducts = await products.getAll(4)
-  const ichimlikProducts = await products.getAll(5) */
   const catigoryId = req.params.id
   const catigoryProducts = await products.getAll(catigoryId - 0)
 
