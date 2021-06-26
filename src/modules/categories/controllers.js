ad78e6d1b1b57c98bdcc9b74c41e9a1f3052980a
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-06-26 21:36:54 +0500

    Add create category

--- src/modules/categories/controllers.js
+++ src/modules/categories/controllers.js
@@ -15,5 +15,100 @@ const categories = require("./model")
 router.get("/", async (req, res) => res.send(await categories.categories()))
 
 
+/*
+  POST categories
+*/
+router.post("/", async(req, res) => {
+
+  try {
+
+    const user = await verify(req.headers.access_token)
+
+    if(user.user_role == 1) {
+
+      const category = await categories.createCategory(req.body)
+
+      if(category) {
+        res.status(201).send(category)
+      }else {
+        res.status(400).end()
+      }
+    }
+    else {
+      res.status(401).end()
+    }
+
+  }
+  catch(err) {
+    res.statusMessage = err
+    res.status(401).end()
+    console.log(err);
+  }
+
+})
+
+
+/*
+  EDIT categories
+*/
+router.put("/:id", async (req, res) => {
+  try {
+
+    const user = await verify(req.headers.access_token)
+
+    if(user.user_role == 1) {
+
+      const category = await categories.editCategory({...req.body, categoryId: req.params.id})
+
+      if(category) {
+        res.status(201).send(category)
+      }else {
+        res.status(400).end()
+      }
+    }
+    else {
+      res.status(401).end()
+    }
+
+  }
+  catch(err) {
+    res.statusMessage = err
+    res.status(401).end()
+    console.log(err);
+  }
+})
+
+
+/*
+  DELETE categories
+*/
+router.delete("/del/:id", async(req, res) => {
+
+  const user = await verify(req.headers.access_token)
+
+  try {
+
+    if(user.user_role == 1) {
+
+      const deleteCatigory = await categories.deleteCatigory(req.params.id)
+    
+      if(deleteCatigory) {
+        res.status(201).send(deleteCatigory)
+      } else {
+        res.status(401).end()
+      }
+    }
+    else {
+      res.status(401).end()
+    }
+  } catch(e) {
+    res.statusMessage = e
+    res.status(401).end()
+    console.log(e);
+  }
+
+})
+
+
 
 module.exports = router
--- src/modules/categories/model.js
+++ src/modules/categories/model.js
@@ -10,5 +10,46 @@ const categoriesSQL = `
 
 const categories = () => rows(categoriesSQL)
 
+/*
+  Create category model
+*/
+const createCategorySQL = `
+  insert into categories(category_name) values ($1) returning *
+`
 
+const createCategory = ({categoryName}) => row(createCategorySQL, categoryName)
+
+
+/*
+  Edit category model
+*/
+const editCategorySQL = `
+  update categories set
+    category_name = coalesce($2, category_name)
+  where
+    category_id = $1
+  returning *
+`
+
+const editCategory = ({categoryName, categoryId}) => row(editCategorySQL, categoryId, categoryName)
+
+
+/*
+  Delete category model
+*/
+const deleteCatigorySQL = `
+  delete from categories
+  where
+    category_id = $1
+  returning *
+`
+const deleteCatigory = (categoryId) => row(deleteCatigorySQL, categoryId)
+
+
+/*
+  EXPORT modules
+*/
 module.exports.categories = categories
+module.exports.createCategory = createCategory
+module.exports.editCategory = editCategory
+module.exports.deleteCatigory = deleteCatigory
